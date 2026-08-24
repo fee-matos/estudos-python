22f197760b4c640c355f2430ea345918c30ee6ad
fernando <feematos21@gmail.com>
2026-08-24 20:59:18 -0300

    feat: Tipo de string

---
+++ aula3.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# aspas simples
+print('Fernando Matos')
+print(1, 'Fernando "Matos"')
+
+# aspas duplas
+print("Fernando Matos")
+print(2, "Fernando 'Matos'")
+
+# escape
+print("Fernando \"Matos\"")
+
+#r
+print(r"Fernando \"Matos\"")
