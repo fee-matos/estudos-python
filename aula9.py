c9e54d3010e8d6726a9ac2a6ca076fcca727840d
fernando <feematos21@gmail.com>
2026-08-25 14:37:51 -0300

    feat: aprendendo sobre operadores aritméticos

---
+++ aula9.py
@@ -0,0 +1,23 @@
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2.2  # float
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 2.2  # corta as casas decimais
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciacão', exponenciacao)
+
+modulo = 55 % 2   # resto da divisão
+print('Módulo', modulo)
+
+print('10 é divisivel de 8?', 10 % 8 == 0)  # testando opcões
+print('16 é divisivel de 8?', 16 % 8 == 0)
