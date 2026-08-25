ae2f28187bdd5712b1dafb31021bf8c03801c4b4
fernando <feematos21@gmail.com>
2026-08-24 23:31:49 -0300

    feat: sobre conversão de tipos (coerção)

---
+++ aula6.py
@@ -0,0 +1,11 @@
+# conversão de tipos, coerção
+# type convertion, typecasting, coercion
+# é o ato de converter um tipo em outro
+# tipos imutáveis e primitivos:
+# str, int, float, bool
+
+print('1', type('1'))
+print(int('1'), type(int('1'))) # converter de str pra int.
+print(int('1') + 1)
+print(bool(' ')) # converter de str pra bool
+print(str(11) + 'b') # converter pra str
