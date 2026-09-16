c54df643cb6b0fae03ef1eeafbb9433a3ac243c7
fernando <feematos21@gmail.com>
2026-09-16 15:03:12 -0300

    feat: aprendendo sobre Operador lógico "or"

---
+++ aula20.py
@@ -0,0 +1,17 @@
+"""
+entrada = input('[E]ntrar [S]air: ')
+senha_digitada = input('Senha: ')
+
+senha_permitida = '123456'
+
+if (entrada == 'E' or entrada == 'e') and senha_digitada == senha_permitida:
+    print('Entrar')
+else:
+    print('Sair') """
+
+
+# avaliação de curto circuito
+print(0 or False or 0 or 'abc' or True)
+
+senha = input('senha: ') or 'Sem senha'
+print(senha)
